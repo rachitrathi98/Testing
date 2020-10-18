6db75a4136fd0ff306297003140bb3d225197ed3
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-18 17:28:09 +0530

    First Commit

---
+++ Testing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
